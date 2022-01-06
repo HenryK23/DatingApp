2823a0fdd62a05752f1cff0b2b95548afccc20e6
HenryK23 <59871978+HenryK23@users.noreply.github.com>
2022-01-06 21:20:01 +0000

    added unit of work pattern, cleaning

--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -11,13 +11,14 @@ namespace API.Interfaces
     public interface IUserRepository
     {
         void Update(AppUser user);
-        Task<bool> SaveAllAsync();
         Task<IEnumerable<AppUser>> GetUsersAsync();
         Task<AppUser> GetUserByIdAsync(int id);
         Task<AppUser> GetUserByUsernameAsync(string Username);
 
         Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams);
         Task<MemberDto> GetMemberByUsernameAsync(string username);
+
+        Task<string> GetUserGender(string username);
         
     }
 }
